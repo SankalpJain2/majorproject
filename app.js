237f14013896ab487c5352a5b62422e92251653f
Sankalp Jain <jainsankalp713@gmail.com>
2024-03-27 18:06:21 +0530

    commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+
+main().then(()=>{
+    console.log("connected");
+}).catch((err)=>{
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/Wanderlust');
+};
+app.get("/",(req,res)=>{
+    res.send("hello");
+});
+
+ app.get("/testListing",async (req,res)=>{
+     let sampleListing = new Listing({
+         title:"my new villa",
+         description:"by the beach",
+         price: 1200,
+         location: "udaipur",
+         country: "india",
+     });
+      await sampleListing.save();
+      console.log("sample was saved");
+      res.send("success");
+ });
+
+app.listen(8080 , ()=>{
+    console.log("server is listening")
+});
